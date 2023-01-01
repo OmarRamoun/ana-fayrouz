592f0ae01ac24a45a3c46f49b3e724348b5d0c78
ramoun <contact@omarramoun.com>
2023-01-01 18:45:16 +0200

    fix: router

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Homepage from '../HomePage';
 
@@ -7,10 +7,10 @@ const AllRoute = () => {
   return (
     <div className='App'>
       <Router>
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/home' component={Homepage} />
-        </Switch>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='home' element={<Homepage />} />
+        </Routes>
       </Router>
     </div>
   );
