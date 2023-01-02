1d077432833296c55e4a0b70be96cc6fe99fa370
ramoun <contact@omarramoun.com>
2023-01-02 21:29:28 +0200

    feat: update modal content styles

--- src/components/Modal/ProjectModalContent.js
+++ src/components/Modal/ProjectModalContent.js
@@ -6,25 +6,40 @@ const ProjectModalContent = ({projectInfo}) => {
     <div className='project-modal-content'>
       <h2 className='project-modal-content__title'>{title}</h2>
       <div className='project-modal-content__info'>
-        <h3 className='project-modal-content__item'>Details: <span className='project-modal-content__item__value'>{details}</span></h3>
-        <h3 className='project-modal-content__item'>Technologies: <span className='project-modal-content__item__value'>{technologies}</span></h3>
         <h3 className='project-modal-content__item'>
-          Preview:{' '}
-          <a href={preview.link} target='_blank' rel='noopener noreferrer'>
-            <span className='project-modal-content__item__value'>{preview.title}</span>
-          </a>
+          Details: <span className='project-modal-content__item__value'>{details}</span>
         </h3>
-        <h3 className='project-modal-content__item'>
-          Preview:{' '}
-          <a href={source.link} target='_blank' rel='noopener noreferrer'>
-            <span className='project-modal-content__item__value'>{source.title}</span>
-          </a>
-        </h3>
-        <h3 className='project-modal-content__item'>
-          Client: <span className='project-modal-content__item__value'>{client}</span>
-        </h3>
+        <div className='row'>
+          <h3 className='project-modal-content__item col-6'>
+            Technologies:{' '}
+            <span className='project-modal-content__item__value'>
+              {technologies
+                .toString()
+                .split(',')
+                .map((item) => ' '.concat(item))
+                .join()}
+            </span>
+          </h3>
+          <h3 className='project-modal-content__item col-6'>
+            Client: <span className='project-modal-content__item__value'>{client}</span>
+          </h3>
+        </div>
+        <div className='row'>
+          <h3 className='project-modal-content__item col-6'>
+            Preview:{' '}
+            <a href={preview.link} target='_blank' rel='noopener noreferrer'>
+              <span className='project-modal-content__item__value'>{preview.title}</span>
+            </a>
+          </h3>
+          <h3 className='project-modal-content__item col-6'>
+            Source:{' '}
+            <a href={source.link} target='_blank' rel='noopener noreferrer'>
+              <span className='project-modal-content__item__value'>{source.title}</span>
+            </a>
+          </h3>
+        </div>
         <div className='project-modal-content__image'>
-          <img src={image} alt="" />
+          <img src={image} alt='' />
         </div>
       </div>
     </div>
