5e03f20cd94b93dcac490d5757f307475a8a0007
Omar Ramoun <contact@omarramoun.com>
2024-02-21 10:15:22 +0200

    build: upate assets and import alias configs

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const { alias } = require('react-app-rewire-alias');
 module.exports = function override(config) {
   alias({
     '@components': 'src/components',
+    '@data': 'src/data',
     '@assets': 'src/assets',
     '@styles': 'src/styles',
     '@lib': 'src/lib',
