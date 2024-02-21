6b1619909b4ff5768f6d8553742af863bb31a4e5
Omar Ramoun <contact@omarramoun.com>
2024-02-21 12:44:21 +0200

    refactor: imports and data

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { Button } from './Button';
+export { Navbar } from './Navbar';
+export { Banner } from './Banner';
+export { About } from './About';
--- src/main-component/HomePage/index.js
+++ src/main-component/HomePage/index.js
@@ -1,12 +1,10 @@
 import React, { Fragment } from 'react';
-import Banner from '../../components/Banner/Banner';
-import About from '../../components/About/About';
 import Service from '../../components/Service/Service';
 import Contact from '../../components/Contact/Contact';
 import Testimonial from '../../components/Testimonial';
 import Portfolio from '../../components/Portfolio/Portfolio';
 import Scrollbar from '../../components/scroolbar';
-import { Navbar } from '../../components/Navbar';
+import { Navbar, Banner, About } from '@components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => (
